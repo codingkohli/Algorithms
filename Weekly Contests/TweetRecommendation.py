9fab96a92153633a597dbf2778c8ea27ad0e4fae
codingkohli <aditya.12.kohli@gmail.com>
2017-11-20 14:08:17 -0500

    Tweet Recommendation for a target User

--- Weekly Contests/TweetRecommendation.py
+++ Weekly Contests/TweetRecommendation.py
@@ -8,4 +8,19 @@
 # 3. Return the list of tweets that cross a threshold
 
 def getRecommendedTweets(followGraph_edges, likeGraph_edges, targetUser, minLikeThreshold):
-    return
+    tweetList = []
+    hashTweet = {}
+    # getting the followers of target user
+    for x,y in followGraph_edges :
+        if x == targetUser :
+            tweetList.append(y)
+    # Getting the liked tweets
+    for x,y in likeGraph_edges :
+        if x in tweetList :
+            try :
+                hashTweet[y] += 1
+            except :
+                hashTweet[y] = 1
+
+    #getting the tweets crossing a threshold
+    return [tweet for tweet in hashTweet if hashTweet[tweet] >= minLikeThreshold]
