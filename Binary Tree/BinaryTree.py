a9b8072afc2f5ae8e5c6bcf598d3debd39eb90f5
codingkohli <aditya.12.kohli@gmail.com>
2017-11-22 11:39:41 -0500

    Adding Pre-order and Pos-Order Traversals

--- Binary Tree/BinaryTree.py
+++ Binary Tree/BinaryTree.py
@@ -1,6 +1,52 @@
-#This file deals with basic methods for creating, deleting, searching and traversals in a binary tree
-class TreeNode():
-    def __init__(self):
-        self.value = None
+#This file deals with basic methods for creating,searching and traversals in a binary tree
+
+#class declaration of a node of a Binary Tree
+class TreeNode:
+    def __init__(self,data):
+        self.value = data
         self.left = None
         self.right = None
+
+# Pre-order Traversal in a Binary Tree : root,left,right {Iterative approach}
+def preOrder(root):
+    if not root:
+        return
+    # the output for the preorder traversal
+    res = []
+    stackList = []
+    res.append(root.value)
+    stackList.append(root.left)
+    stackList.append(root.right)
+    while(stackList):
+        temp = stackList.pop(0)
+        if temp :
+            res.append(temp.value)
+            stackList.append(temp.left)
+            stackList.append(temp.right)
+
+    return res
+
+# Post-order Traversal in a Binary Tree : left,right,root {Recursive approach}
+def postOrder(root):
+    if root :
+        # the output for the traversal
+        outRes = []
+        outRes.append(postOrder(root.left))
+        outRes.append(postOrder(root.right))
+        outRes.append(root.value)
+        return outRes
+
+# Creating a basic Binary Tree
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+# printing the pre-order Traversal
+print(preOrder(root))
+
+#printing the post-order Traversal
+print(postOrder(root))
