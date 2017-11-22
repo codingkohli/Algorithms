09b645c39a4a1a1310c70712b4c955945d642738
codingkohli <aditya.12.kohli@gmail.com>
2017-11-22 11:00:45 -0500

    Creation of Binary Tree

---
+++ Binary Tree/BinaryTree.py
@@ -0,0 +1,6 @@
+#This file deals with basic methods for creating, deleting, searching and traversals in a binary tree
+class TreeNode():
+    def __init__(self):
+        self.value = None
+        self.left = None
+        self.right = None
